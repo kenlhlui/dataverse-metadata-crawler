d4c9908d2767e6f3948b960afaf9281cb784c2f7
Ken Lui <116421546+kenlhlui@users.noreply.github.com>
2026-05-29 12:43:21 -0400

    feat: change the structure of the returned dataset metadata and permission

--- dvmeta/crawler/new_crawler.py
+++ dvmeta/crawler/new_crawler.py
@@ -94,7 +94,7 @@ class MetaDataCrawler:
 
         return datasets
 
-    async def get_dataset_metadata(self, dataset_ids: list, draft: bool = False) -> list:
+    async def get_dataset_metadata(self, dataset_ids: list, draft: bool = False) -> dict:
         """Get the metadata of a dataset using the dataset Native API endpoint.
 
         Args:
@@ -102,29 +102,29 @@ class MetaDataCrawler:
             draft (bool): Whether to fetch the draft version
 
         Returns:
-            list: A list of dataset metadata dictionaries
+            dict: A dictionary mapping dataset IDs to their metadata
         """
         url_list = [self.endpoints.ds_json(dataset_id, draft=draft) for dataset_id in dataset_ids]
 
         response = await self.client.async_get(url_list)
 
-        return [res.json() for res in response if res is not None]
+        return {
+            dataset_id: res.json() for dataset_id, res in zip(dataset_ids, response, strict=False) if res is not None
+        }
 
-    async def get_dataset_permissions(self, dataset_ids: list) -> list:
+    async def get_dataset_permissions(self, dataset_ids: list) -> dict:
         """Get the permission metadata of a dataset using the dataset permissions API endpoint.
 
         Args:
             dataset_ids (list): A list of dataset (entity) IDs
 
         Returns:
-            list: A list of dataset permission metadata dictionaries
+            dict: A dictionary mapping dataset IDs to their permission metadata
         """
         url_list = [self.endpoints.ds_permissions(dataset_id) for dataset_id in dataset_ids]
 
         response = await self.client.async_get(url_list)
 
-        return [
-            {**res.json(), 'entity_id': dataset_id}
-            for dataset_id, res in zip(dataset_ids, response, strict=False)
-            if res is not None
-        ]
+        return {
+            dataset_id: res.json() for dataset_id, res in zip(dataset_ids, response, strict=False) if res is not None
+        }
